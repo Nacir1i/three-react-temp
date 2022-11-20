10afd0286f6e5fe40ad0ac6ad91351d42135a627
Nacir1i <113454578+Nacir1i@users.noreply.github.com>
2022-11-20 03:11:27 -0800

    set up first cube with threejs and a basic spining animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import * as THREE from "three";
+
 export default function App() {
-  return (
-    <div className="w-sreen h-screen">
-     
-    </div>
-  )
+  useEffect(() => {
+    const camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      10
+    );
+    camera.position.z = 1;
+    const scene = new THREE.Scene();
+    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const material = new THREE.MeshNormalMaterial();
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animation);
+    document.body.appendChild(renderer.domElement);
+    function animation(time) {
+      mesh.rotation.x = time / 2000;
+      mesh.rotation.y = time / 1000;
+      renderer.render(scene, camera);
+    }
+  }, []);
+
+  return <div className="bg-red-400"></div>;
 }
